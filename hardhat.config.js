c425dfb88c729eaf1341763c721aba7ce1fc27d6
Ernest Nnamdi <ernest@Ernests-MacBook-Air.local>
2021-11-10 21:27:22 +0100

    connect wallet and wave

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,11 @@
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/7pLMT-KkA1jLVr_VRfMtk5wCSiNYI39P",
+      accounts: ["630d0799eb839a3b6ea728202b7a26b137f621bcd4d797e9da4150be22ba7ae1"]
+    }
+  }
 };
